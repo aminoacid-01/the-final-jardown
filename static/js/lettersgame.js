657416991a48ca561cb4f9df5b3c22b68a2be922
NewWorldHost <jeffries839@gmail.com>
2025-02-13 17:18:58 +0000

    add javascript for buttons

--- static/js/lettersgame.js
+++ static/js/lettersgame.js
@@ -1,24 +1,40 @@
-let selectedLetters = [];
-
-document.getElementById('vowel-btn').addEventListener('click', async () => {
-    if (selectedLetters.length < 9) {
-        const response = await fetch('/random-vowel/');
-        const randomVowel = await response.json();
-        selectedLetters.push(randomVowel);
-        updateSelectedLetters();
-    }
-});
-
-document.getElementById('consonant-btn').addEventListener('click', async () => {
-    if (selectedLetters.length < 9) {
-        const response = await fetch('/random-consonant/');
-        const randomConsonant = await response.json();
-        selectedLetters.push(randomConsonant);
-        updateSelectedLetters();
-    }
-});
-
-function updateSelectedLetters() {
-    const selectedLettersDiv = document.getElementById('selected-letters');
-    selectedLettersDiv.innerHTML = selectedLetters.join(' ');
+const lettersGame = {
+    selectedLetters: [],
+    vowels: ['A', 'E', 'I', 'O', 'U'],
+    consonants: 'BCDFGHJKLMNPQRSTVWXYZ'.split(''),
+    
+const vowelBtn = document.getElementById('vowel-btn');
+if (vowelBtn) {
+    vowelBtn.addEventListener('click', () => {
+        if (selectedLetters.length < 9) {
+            const randomVowel = vowels[Math.floor(Math.random() * vowels.length)];
+            selectedLetters.push(randomVowel);
+            updateSelectedLetters();
+        }
+    });
 }
+    },
+
+    addConsonant() {
+        if (this.selectedLetters.length < 9) {
+            const randomConsonant = this.consonants[Math.floor(Math.random() * this.consonants.length)];
+            this.selectedLetters.push(randomConsonant);
+            this.updateSelectedLetters();
+        }
+    },
+
+    updateSelectedLetters() {
+        const selectedLettersDiv = document.getElementById('selected-letters');
+        selectedLettersDiv.innerHTML = this.selectedLetters.join(' ');
+const vowelBtn = document.getElementById('vowel-btn');
+if (vowelBtn) {
+    vowelBtn.addEventListener('click', () => lettersGame.addVowel());
+}
+
+const consonantBtn = document.getElementById('consonant-btn');
+if (consonantBtn) {
+    consonantBtn.addEventListener('click', () => lettersGame.addConsonant());
+}
+
+document.getElementById('vowel-btn').addEventListener('click', () => lettersGame.addVowel());
+document.getElementById('consonant-btn').addEventListener('click', () => lettersGame.addConsonant());
