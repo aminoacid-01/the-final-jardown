553d756157675eff3c18e2901844f32726fc469b
NewWorldHost <jeffries839@gmail.com>
2025-02-13 16:58:54 +0000

    remove excess code no longer needed

--- countdown/views.py
+++ countdown/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-import random 
-
-#arrays have to be here.
-vowels = ['a', 'e', 'i', 'o', 'u']
-consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+from django.http import HttpResponse
 
 
 # Create your views here.
 
-def lettersGame(request):
-    return render(request, 'lettersGame.html')
-
-#lettersgame views
-def random_vowel(request):
-    return JsonResponse(random.choice(vowels), safe=False)
-
-def random_consonant(request):
-    return JsonResponse(random.choice(consonants), safe=False)
